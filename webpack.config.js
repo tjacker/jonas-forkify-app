c6d9ffb7d7b7c1eb1b909f849d6b922970e854db
Tim Acker <tjacker31@gmail.com>
2018-09-30 21:44:32 -0400

    Adds build and devserver settings to webpack and scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/index.js',
 	output: {
-		path: path.resolve(__dirname, 'dist/js'),
-		filename: 'bundle.js'
-	}
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'js/bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		})
+	]
 };
