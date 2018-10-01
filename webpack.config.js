b678857df911c1f125ed846930e900eb817ec263
Tim Acker <tjacker31@gmail.com>
2018-09-30 22:32:59 -0400

    Adds babel loader and configuration to project

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-	entry: './src/js/index.js',
+	entry: ['babel-polyfill', './src/js/index.js'],
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'js/bundle.js'
@@ -15,5 +15,16 @@ module.exports = {
 			filename: 'index.html',
 			template: './src/index.html'
 		})
-	]
+	],
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
 };
