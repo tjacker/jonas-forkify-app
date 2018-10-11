b8df5a1f6e6601196388d77db0dde77c784bbba6
Tim Acker <tjacker31@gmail.com>
2018-10-10 22:36:26 -0400

    Creates basic state object to store results

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,26 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-// search.getResults();
+// Store global state of the app
+const state = {};
+
+const controlSearch = async () => {
+	// Get query from view
+	const query = 'pizza'; //TODO:
+
+	if (query) {
+		// New search object to add to state
+		state.search = new Search(query);
+		// Prepare UI for results
+
+		// Search for recipes
+		await state.search.getResults();
+
+		// Render results in UI
+		console.log(state.search.result);
+	}
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+	e.preventDefault();
+	controlSearch();
+});
